65017de441c733594a3b424fa60c19a82392f677
rashmoni <rashudey9851@gmail.com>
2022-10-22 22:03:20 +0200

    add few stuffs

---
+++ src/test/java/com/novare/natflix/NatflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novare.natflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
